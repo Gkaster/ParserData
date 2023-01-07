2349668cbd399e37dcbcea41b0fcaa2f4fdaa012
Mikhail Nakonechnyi <mykhail.nakonechnyi@gmail.com>
2023-01-07 19:01:56 +0000

    site cookiecutter generated

---
+++ site_dashboard/site_dashboard/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from site_dashboard.users.models import User
+from site_dashboard.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
