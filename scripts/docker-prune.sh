e73f5aad8ecf5f5ce8977b3ffc3c7cee6c8df468
root <root@disp3269>
2023-01-08 01:53:18 +0000

    original and working started template

---
+++ scripts/docker-prune.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#stop all containers
+docker stop $(docker ps -a -q)
+# docker ps -a
+docker rm -f $(docker ps -aq)
+docker kill $(docker ps -q)
+docker system prune -a --volumes
+docker system prune -a -f
+docker rmi $(docker images -q -f dangling=true)
+#docker rmi $(docker images -a -q)
+
+docker rm $(docker ps -aq)
+
+
+
+
+
+# docker rm $(docker ps -qa)
