0522f8fccb873f0b6b24820d82dd829341772dc4
Mikhail Nakonechnyi <mykhail.nakonechnyi@gmail.com>
2023-01-21 23:52:46 +0000

    docker cleaner script update

--- scripts/docker-prune.sh
+++ scripts/docker-prune.sh
@@ -7,10 +7,10 @@ docker rm -f $(docker ps -aq)
 docker kill $(docker ps -q)
 #docker system prune -a --volumes
 #docker system prune -a -f
-docker rmi $(docker images -q -f dangling=true)
+# docker rmi $(docker images -q -f dangling=true)
 #docker rmi $(docker images -a -q)
 
-docker rm $(docker ps -aq)
+#docker rm $(docker ps -aq)
 
 
 
